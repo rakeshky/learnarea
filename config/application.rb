63e4b7e0dcfa3c592eddfc550f517088bcf2b207
rakeshky <498.rakeshyadav@gmail.com>
2013-04-04 11:13:22 +0530

    first learnarea commit

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,6 @@ module Learnarea
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.precompile += %w(showreports.css showreports.js)
   end
 end
